45b945ac7db467c0f376afdfc7927b1d83063be6
Michael Chan <10224558+chanman04@users.noreply.github.com>
2019-05-12 02:54:09 -0500

    updated the retrieve web resource

--- BasicRetrieveStudent.js
+++ BasicRetrieveStudent.js
@@ -4,7 +4,7 @@ function retrieveStudent() {
     Xrm.WebApi.retrieveRecord("new_student", "E00EE086-3673-E911-A984-000D3A3AC0C2", "?$select=new_name,new_cumulativegpa").then(
         function success(result) {
 
-            alert("Student:" +result.new_name, "Cumulative GPA:" +result.new_cumulativegpa);
+            alert("Student:" + result.new_name + ",Cumulative GPA:" + result.new_cumulativegpa);
         },
         function (error) {
             console.log('error.mesage');
