7d5a5e1c0d8f5461a2ec6e6d29ee0aa9942fa466
Michael Chan <10224558+chanman04@users.noreply.github.com>
2019-05-12 02:26:32 -0500

    pushing up API retrieve web resource

---
+++ BasicRetrieveStudent.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+function retrieveStudent() {
+
+    Xrm.WebApi.retrieveRecord("new_student", "E00EE086-3673-E911-A984-000D3A3AC0C2", "?$select=new_name,new_cumulativegpa").then(
+        function success(result) {
+
+            alert("Student:" +result.new_name, "Cumulative GPA:" +result.new_cumulativegpa);
+        },
+        function (error) {
+            console.log('error.mesage');
+        }
+
+    );
+}
